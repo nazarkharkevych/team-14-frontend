92208077f97c17389b286661278da950331afdbf
Nazar Kharkevych <nkharkevych@gmail.com>
2023-11-10 13:22:40 +0200

    Added map

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from "react";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 export const App = () => {
   return (
     <div className="App">
-      Hello team-14
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 };
